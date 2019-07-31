38b74a9a3ef731eb6ff3cd4ae4c1aa8338fb0cca
Ian <41586905+ianbaxter@users.noreply.github.com>
2019-07-31 14:39:43 +0100

    Fix VH bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,3 @@
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 // jQuery dropdown animations
 $('nav li').hover(
     function() {
