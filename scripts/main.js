3dc4cf8b6785a91716367513bc8e0615c447076a
Ian <41586905+ianbaxter@users.noreply.github.com>
2020-03-20 15:57:45 +0000

    remove jquery and change scrolling
    remove unnecessary jquery and change from using JS to add scroll behaviour to using css property: scroll-behavior

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ const navToggle = () => {
   const navBar = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
   const body = document.querySelector("body");
-  const navLink = document.querySelectorAll(".nav-link");
 
   // Add transform to menu after page load to prevent initial menu transform
   navBar.classList.add("nav-transform");
@@ -14,10 +13,10 @@ const navToggle = () => {
     navMenuToggle();
   });
 
-  navLink.forEach(link =>
+  navLinks.forEach(link =>
     link.addEventListener("click", () => {
-      if ($(window).width() <= 600) {
-        // Toggle navigation menu if link clicked in mobile view
+      if (window.innerWidth <= 700) {
+        // Close navigation menu if link clicked in mobile view
         navMenuToggle();
       }
     })
@@ -44,47 +43,6 @@ const navToggle = () => {
     // Navigation menu animation
     menu.classList.toggle("toggle");
   }
-
-  // Select all links with hashes
-  // Remove links that don't actually link to anything
-  $(
-    'a[href*="#"]:not([href="#"]):not([href="#0"]):not([href="#carouselExampleIndicators"])'
-  ).click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top
-          },
-          1000,
-          function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) {
-              // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            }
-          }
-        );
-      }
-    }
-  });
 };
 
 navToggle();
