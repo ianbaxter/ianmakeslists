b57c9477c3813d078aef58d491c811634a2edd8d
Ian <41586905+ianbaxter@users.noreply.github.com>
2020-06-16 21:22:35 +0100

    add secret page

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ const gulp = require("gulp");
 const babel = require("gulp-babel");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
 
-gulp.task("processJS", function () {
+gulp.task("imagemin", () => {
+  return gulp.src("src/img/*").pipe(imagemin()).pipe(gulp.dest("src/imgmin"));
+});
+
+gulp.task("processJS", () => {
   return gulp
     .src("src/scripts/*.js")
     .pipe(babel())
@@ -14,4 +19,5 @@ gulp.task("processJS", function () {
 
 gulp.task("watch", function () {
   gulp.watch("src/scripts/*.js", gulp.series("processJS"));
+  gulp.watch("src/img/*", gulp.series("imagemin"));
 });
